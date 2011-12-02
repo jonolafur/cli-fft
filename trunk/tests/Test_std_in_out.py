aab45edad820bda2786e2f12e44c5c13fd388242
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2011-12-02 16:02:25 +0000

    Working on getting cmake to behave under win and linux...

--- trunk/tests/Test_std_in_out.py
+++ trunk/tests/Test_std_in_out.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 # Since CTest does not support file re-directing we need this
 ## somewhat awkward wrapper to test the re-directing supported be fft:
-import string, sys, os
+import sys, subprocess
 
-os.system('../fft -c -x 1 -y 2 -N < Test_read_file.txt > Test_fft_std_in_out.txt')
+fft_command = sys.argv[1]+'/debug/fft'
+test_file_in=sys.argv[1]+'/tests/Test_read_file.txt'
+test_file_out=sys.argv[1]+'/tests/Test_fft_std_in_out.txt'
+print(fft_command)
+print(test_file_in)
+print(test_file_out)
+
+subprocess.call([fft_command, "-c -x 1 -y 2 -N < ", test_file_in, " > ", test_file_out])
 
