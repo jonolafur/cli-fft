605d6642b3adf4d8ab6b642ad260a6a8e8ea80b4
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2012-03-25 16:27:07 +0000

    Completely re-factored all member variables out of fftOptions. These are contained in variable map in boost::program_options. All unit tests still pass.

--- trunk/src/fftOptions.h
+++ trunk/src/fftOptions.h
@@ -13,30 +13,29 @@
 class fftOptions : public programOptions
 {
 public:
-	std::string inFileName, outFileName;
-	int xColumn;
-	int yColumn;
-	char delimiter;
-	bool magnitude_phase;
-	bool inverse;
-
-public:
 	fftOptions(int argc, char* argv[]);
 	virtual ~fftOptions(){};
 
 	std::vector<int> getZeroBasedColumnIndexes();
 	std::string getDelimiterAsString();
 	bool isComplex() const {return var_map.count("complex")!=0;}
-	bool xAxisSpecified() const {return xColumn!=0;}
+	std::string inputFile();
+	std::string outputFile();
+	char delimiter(){return var_map["delimiter"].as<char>();}
+	bool xAxisSpecified() const {return x_value_Idx()!=0;}
 	bool orderSamples() const {return (var_map.count("ordered-x-axis")!=0 && !positiveAxisOnly() );}
 	bool positiveAxisOnly() const {return ( var_map.count("positive-only")!=0 && !isComplex() );}
 	bool normalize() const {return var_map.count("normalize")!=0;}
 
-	bool writeMagnitudeAndPhase() const {return magnitude_phase;}
-	bool inverseFFT() const {return inverse;}
+	bool writeMagnitudeAndPhase() const {return var_map.count("magnitude-phase") !=0;}
+	bool inverseFFT() const {return var_map.count("inverse") !=0;}
 
 protected:
 	void addfftOptions();
+
+private:
+	int x_value_Idx() const { return var_map["x-values"].as<int>(); };
+	int y_value_Idx() const { return var_map["y-values"].as<int>(); };
 
 };
 
