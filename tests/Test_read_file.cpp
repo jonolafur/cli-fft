ab3cff4e6096b2ddcb06c9c9094955d87c00fce6
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2012-04-06 14:36:20 +0000

    Adding unit tests to further check file IO

--- tests/Test_read_file.cpp
+++ tests/Test_read_file.cpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include "fileIO.h"
 
+///////////////////////////////////////////////////////////////////////////////
 void generateFile(std::string fileName, int numLines)
 {
 	std::cout << "Generating data file: " << fileName
@@ -37,12 +38,76 @@ void generateFile(std::string fileName, int numLines)
 	std::cout << "Finished generating data file." << std::endl;
 }
 ///////////////////////////////////////////////////////////////////////////////
+bool readFile( std::string fileName, std::vector<int> columnIdx, std::size_t numberOfLines )
+{
+	data_columns columns;
+	std::string delimiter = " ";
+
+	getColsFromFile(fileName, columns, columnIdx, delimiter);
+
+	if(columns.size() != columnIdx.size())
+	{
+		std::cout << "Unable to retrieve the required number of columns.\n";
+		return false;
+	}
+
+	if(!columns.checkEqualNonzeroColumnLength())
+	{
+		std::cout << "Unequal columns detected.\n";
+		return false;
+	}
+
+	if(columns.max_size() != numberOfLines)
+	{
+		std::cout << "Incorrect number of entries read. Should have been: "
+				<< numberOfLines << " but " << columns.max_size() << " were retrieved.\n";
+		return false;
+	}
+
+	for(std::size_t i = 0; i<columns.size(); i++)
+	{
+		if(columnIdx[i]<0 && columns.m_data[i].size()!=0)
+		{
+			std::cout << "Index for column nr. " << i
+					<< " was negative, but the corresponding column contains "
+					<< columns.m_data[i].size() << " entries, should have been zero.\n";
+			return false;
+		}
+	}
+
+	return true;
+}
+///////////////////////////////////////////////////////////////////////////////
+bool checkColumnLength(std::string fileName, std::size_t numberOfLines )
+{
+	data_columns columns;
+	std::string delimiter = " ";
+	std::vector<int> columnIdx;
+
+	columnIdx.push_back(1);
+	columnIdx.push_back(2);
+	columnIdx.push_back(3);
+
+	getColsFromFile(fileName, columns, columnIdx, delimiter);
+
+	// Change the size of one of the non-zero length cols:
+	columns.m_data[0].resize(numberOfLines*2);
+
+	if(!columns.checkEqualNonzeroColumnLength())
+	{
+		std::cout << "Unequal columns correctly detected.\n";
+		return true;
+	}
+
+	std::cout << "Unequal columns should have been detected.\n";
+	return false;
+}
+///////////////////////////////////////////////////////////////////////////////
 int Test_read_file(int, char*[] )
 {
 	int ret_ok=0, ret_fail=1;
 	std::size_t numberOfLines = 2000;
 
-	data_columns columns;
 	std::vector<int> columnIdx;
 	std::string delimiter = " ";
 	std::string fileName("Test_read_file.txt");
@@ -51,29 +116,23 @@ int Test_read_file(int, char*[] )
 	columnIdx.push_back(2);
 	columnIdx.push_back(3);
 
-
 	generateFile(fileName, numberOfLines);
 
-	getColsFromFile(fileName, columns, columnIdx, delimiter);
-
-	if(columns.size() != columnIdx.size())
-	{
-		std::cout << "Unable to retrieve the required number of columns.\n";
+	if( !readFile(fileName,columnIdx,numberOfLines) )
 		return ret_fail;
-	}
 
-	if(!columns.checkEqualNonzeroColumnLength())
-	{
-		std::cout << "Unequal columns detected.\n";
-		return ret_fail;
-	}
+	columnIdx[0] = -1;
 
-	if(columns.max_size() != numberOfLines)
-	{
-		std::cout << "Incorrect number of entries read. Should have been: "
-				<< numberOfLines << " but " << columns.max_size() << " were retrieved.\n";
+	if( !readFile(fileName,columnIdx,numberOfLines) )
 		return ret_fail;
-	}
+
+	columnIdx[2] = -1;
+
+	if( !readFile(fileName,columnIdx,numberOfLines) )
+		return ret_fail;
+
+	if( !checkColumnLength(fileName, numberOfLines) )
+		return ret_fail;
 
 	return ret_ok;
 }
