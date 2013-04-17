6866ccb644bf3824b9458ef1785e1ec65b2926e8
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2013-04-17 15:35:59 +0000

    Cleaning up logging mechanism. Does not compile, work in progress

--- trunk/src/LogStreamHandler.cpp
+++ trunk/src/LogStreamHandler.cpp
@@ -7,12 +7,53 @@
 
 #include "LogStreamHandler.h"
 
-LogStreamHandler::LogStreamHandler() {
-	// TODO Auto-generated constructor stub
-
+LogStreamHandler::LogStreamHandler(): log_file_name(".fft.log"), log_file(), backup(NULL)
+{
+   log_file_name = makeLogPath(log_file_name);
+   backup = redirect_clog(log_file_name, log_file );
 }
 
-LogStreamHandler::~LogStreamHandler() {
-	// TODO Auto-generated destructor stub
+LogStreamHandler::~LogStreamHandler()
+{
+   std::clog.rdbuf(backup);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+void LogStreamHandler::redirect_clog(std::string log_path,
+                              std::ofstream& log_file)
+{
+   std::streambuf* clog_buffer = std::clog.rdbuf();
+
+   log_file.open(log_path.c_str());
+
+   if(log_file)
+      std::clog.rdbuf( log_file.rdbuf() );
+   else
+   {
+      std::string s = "Failed to open file: " + log_path + "for writing.\n";
+      throw s;
+   }
+
+   return clog_buffer;
+}
+///////////////////////////////////////////////////////////////////////////////
+voi LogStreamHandler::makeLogPath(std::string log_file_base_name)
+{
+   bool readHomePathOk=false;
+   std::string log_path;
+
+   char* homePath = getenv("HOME");
+
+   if(homePath)
+   {
+      readHomePathOk=true;
+      log_path = std::string( homePath ) + "/" + log_file_base_name;
+   }
+   else
+      log_path = log_file_base_name;
+
+   if(!readHomePathOk)
+      std::cout << "# Unable to retrieve home path. Writing log to current directory.\n";
+
+   return log_path;
+}
--- trunk/src/LogStreamHandler.h
+++ trunk/src/LogStreamHandler.h
@@ -8,10 +8,25 @@
 #ifndef LOGSTREAMHANDLER_H_
 #define LOGSTREAMHANDLER_H_
 
+//////////////     Include section     ////////////////////////////////////////
+// STL headers:
+#include <string>
+#include <iostream>
+#include <iomanip>
+
 class LogStreamHandler {
+private:
+   std::string      log_file_name;
+   std::ofstream    log_file;
+   std::streambuf*  backup;
+
 public:
 	LogStreamHandler();
 	virtual ~LogStreamHandler();
+   
+   void redirect_clog( std::string log_path, std::ofstream& log_file );
+   voi makeLogPath( std::string log_file_base_name );
+
 };
 
 #endif /* LOGSTREAMHANDLER_H_ */
