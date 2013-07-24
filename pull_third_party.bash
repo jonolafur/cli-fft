e8544d0557bf818cc35ec31349d84a6284a12de6
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2013-07-24 22:14:23 +0000

    pull_in_third_party now seems to work. Still trying to get it to work for all targets (Win32, win64, Lin32 and Lin 64)

--- pull_third_party.bash
+++ pull_third_party.bash
@@ -1,13 +1,27 @@
 #!/bin/bash
+## Build date of MinGW-64 tool chain
+MINGW_BUILD_DATE="20130622"
+BOOST_MIN_VER="54" #Currently using boost 1.54.0
+HOST_ARCH="x86_64" #i686
+TARGET_ARCH="i686"
 
 ## Retrieveing the local directory:
 CURR_DIR=`pwd`
-BUILD_DIR="${CURR_DIR}/build"
-TC_ROOT="${BUILD_DIR}/tool-chains"
-LIB_ROOT="${BUILD_DIR}/libraries"
-DOWNLOADS="${BUILD_DIR}/downloads"
+BUILD_ROOT="${CURR_DIR}/build"
 
-mkdir ${BUILD_DIR}
+if [ -d ${BUILD_ROOT} ]
+then
+  echo "Placing all tool-chain related artefacts in ${BUILD_ROOT}"
+else
+  echo "Creating build root directory."
+  mkdir -p ${BUILD_ROOT}
+fi
+
+TC_ROOT="${BUILD_ROOT}/tool-chains"
+LIB_ROOT="${BUILD_ROOT}/libraries"
+DOWNLOADS="${BUILD_ROOT}/downloads"
+
+mkdir ${BUILD_ROOT}
 mkdir -p ${TC_ROOT}
 mkdir -p ${LIB_ROOT}
 mkdir -p ${DOWNLOADS}
@@ -17,27 +31,30 @@ echo "Third party libs will be located here: ${LIB_ROOT}"
 echo "Downloaded files will be located here: ${DOWNLOADS}"
 
 ###############################################################
-## mingw32-w64 tool chain.
-TC_MINGW32_URL="http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Automated%20Builds"
-TC_MINGW32_PREFIX="i686-w64-mingw32"
-TC_MINGW32_TAR_BALL="mingw-w32-bin_i686-linux_20130515.tar.bz2"
-TC_MINGW32_ROOT="${TC_ROOT}/mingw-w32-bin_i686-linux"
+## 32 bit target mingw-w64 tool chain.
+TC_MINGW_URL="http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Automated%20Builds"
+TC_ID="mingw-w32-bin_${HOST_ARCH}-linux"
+TC_MINGW_PREFIX="${TARGET_ARCH}-w64-mingw32"
+TC_MINGW_TAR_BALL="${TC_ID}_${MINGW_BUILD_DATE}.tar.bz2"
+TC_MINGW_ROOT="${TC_ROOT}/${TC_ID}"
 
-echo "Retrieving the ${TC_MINGW32_PREFIX} tool chain using wget..."
+echo "Retrieving the ${TC_MINGW_PREFIX} tool chain using wget..."
 
-wget --continue --directory-prefix=${DOWNLOADS} ${TC_MINGW32_URL}/${TC_MINGW32_TAR_BALL}
+wget --continue --directory-prefix=${DOWNLOADS} ${TC_MINGW_URL}/${TC_MINGW_TAR_BALL}
 
-mkdir -p ${TC_MINGW32_ROOT}
+mkdir -p ${TC_MINGW_ROOT}
 
-echo "Extracting the mingw-w32-bin_i686-linux tool chain..."
+echo "Extracting the ${TC_ID} tool chain..."
 
-tar --directory ${TC_MINGW32_ROOT} -xf ${DOWNLOADS}/${TC_MINGW32_TAR_BALL}
+tar --directory ${TC_MINGW_ROOT} -xf ${DOWNLOADS}/${TC_MINGW_TAR_BALL}
 
 ###############################################################
 ## Get Boost:
-BOOST_URL="http://sourceforge.net/projects/boost/files/boost/1.52.0"
-BOOST_TAR_BALL="boost_1_52_0.tar.bz2"
-BOOST_ROOT_DIR="${LIB_ROOT}/boost_1_52_0"
+BOOST_ID="1_${BOOST_MIN_VER}_0"
+BOOST_VER="1.${BOOST_MIN_VER}.0"
+BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VER}"
+BOOST_TAR_BALL="boost_${BOOST_ID}.tar.bz2"
+BOOST_ROOT_DIR="${LIB_ROOT}/boost_${BOOST_ID}"
 
 wget --continue --directory-prefix=${DOWNLOADS} ${BOOST_URL}/${BOOST_TAR_BALL}
 
@@ -56,11 +73,11 @@ then
    ## after the "import feature ;" line: 
    cp project-config.jam tmp.jam
    cat tmp.jam | sed "/import feature ;/a\
-                       using gcc : mingw : ${TC_MINGW32_ROOT}/bin/${TC_MINGW32_PREFIX}-g++ ;" > project-config.jam
+                       using gcc : mingw : ${TC_MINGW_ROOT}/bin/${TC_MINGW_PREFIX}-g++ ;" > project-config.jam
    rm tmp.jam
    ## Building boost libraries with bjam:
    ./b2 -a toolset=gcc target-os=windows --layout=tagged --with-program_options\
-        --prefix=${TC_MINGW32_ROOT}/${TC_MINGW32_PREFIX}/ install
+        --prefix=${TC_MINGW_ROOT}/${TC_MINGW_PREFIX}/ install
    cd  ${CURR_DIR} # Back to the original 
 else
    echo "Failed to locate the boost root directory: ${BOOST_ROOT_DIR}"
@@ -82,9 +99,9 @@ if [ -d ${FFTW_ROOT_DIR} ]
 then
    echo "Changing into the fftw root directory: ${FFTW_ROOT_DIR}"
    cd ${FFTW_ROOT_DIR}
-   ./configure CC=${TC_MINGW32_ROOT}/bin/${TC_MINGW32_PREFIX}-gcc \
-        --prefix=${TC_MINGW32_ROOT}/${TC_MINGW32_PREFIX}\
-        --host=${TC_MINGW32_PREFIX}\
+   ./configure CC=${TC_MINGW_ROOT}/bin/${TC_MINGW_PREFIX}-gcc \
+        --prefix=${TC_MINGW_ROOT}/${TC_MINGW_PREFIX}\
+        --host=${TC_MINGW_PREFIX}\
         -with-our-malloc --with-windows-f77-mangling --disable-shared --enable-static --enable-threads\
         --with-combined-threads --with-incoming-stack-boundary=2
    make -j
@@ -100,15 +117,15 @@ fi
 ## Prepare tool chain file for cmake:
 ## Constructing the tool chain file from the template mingw.cmake.template. Here we need to
 ## insert the corresponding values for the tool chain:
-echo "Generating a cmake tool chain file for ${TC_MINGW32_PREFIX}:"
+echo "Generating a cmake tool chain file for ${TC_MINGW_PREFIX}:"
 
-cat mingw.cmake.template | sed -e "s:set(TC_PREFIX:& ${TC_MINGW32_PREFIX}:" \
-                               -e "s:set(CMAKE_FIND_ROOT_PATH:& ${TC_MINGW32_ROOT} ${TC_MINGW32_ROOT}/${TC_MINGW32_PREFIX}:"\
+cat mingw.cmake.template | sed -e "s:set(TC_PREFIX:& ${TC_MINGW_PREFIX}:" \
+                               -e "s:set(CMAKE_FIND_ROOT_PATH:& ${TC_MINGW_ROOT} ${TC_MINGW_ROOT}/${TC_MINGW_PREFIX}:"\
                                > mingw32.cmake
 mkdir -p build/mingw32
 cd build/mingw32
 
-echo "Starting cmake with the mingw32.cmake tool chain file to build for ${TC_MINGW32_PREFIX}:"
+echo "Starting cmake with the mingw32.cmake tool chain file to build for ${TC_MINGW_PREFIX}:"
 
 rm -rf * && cmake -DCMAKE_TOOLCHAIN_FILE=../../mingw32.cmake -DCMAKE_BUILD_TYPE=release ../../
 
