cda43a0971ae3ea926516dd3be16ebf3834145f6
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2013-09-22 15:10:34 +0000

    Adding better path handling in build_native.bash script.

--- trunk/build_native.bash
+++ trunk/build_native.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function build_target() {
-  BUILD_DIR="$1"
-  BUILD_TYPE="$2"
+  local BUILD_DIR="$1"
+  local BUILD_TYPE="$2"
 
   ################################################################
   ## SVN version:
   ################################################################
-  VERSION=`svnversion .`
+  VERSION=`svnversion ${SRC_DIR}`
   echo "SVN Version to be built: ${VERSION}"
 
   if [ ! -d ${BUILD_DIR} ];
@@ -18,22 +18,45 @@ function build_target() {
 
   cd ${BUILD_DIR}
   rm -rf *
-  cmake ../../ -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-  make
+  cmake ${SRC_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+  make -j 4
 
 }
 
+SRC_DIR=`readlink -e $1`
+ROOT_BUILD_DIR="$2"
+
+if [ ! -d ${SRC_DIR} ];
+then
+  echo "The directory: ${SRC_DIR} does not exist."
+  echo "Please select a valid directory containing the sources as a first argument."
+  exit 1
+fi
+
+if [ ! -d ${ROOT_BUILD_DIR} ];
+then
+  echo "Creating root build directory: ${ROOT_BUILD_DIR}"
+  mkdir -p ${ROOT_BUILD_DIR}
+fi
+
+ROOT_BUILD_DIR=`readlink -e ${ROOT_BUILD_DIR}`
+
+echo "Build root dir: ${ROOT_BUILD_DIR}"
+
+## Checking machine architecture and setting directories according to architecture:
 ARCH=`uname -i`
-ROOT_DIR=`pwd`
-BUILD_DIR_RELEASE="${ROOT_DIR}/build/release32"
-BUILD_DIR_DEBUG="${ROOT_DIR}/build/debug32"
+BUILD_DIR_RELEASE="${ROOT_BUILD_DIR}/release32"
+BUILD_DIR_DEBUG="${ROOT_BUILD_DIR}/debug32"
 
 echo "Building on a $ARCH machine"
 if [ "$ARCH" == "x86_64" ];
 then
-  BUILD_DIR_RELEASE="${ROOT_DIR}/build/release64"
-  BUILD_DIR_DEBUG="${ROOT_DIR}/build/debug64"
+  BUILD_DIR_RELEASE="${ROOT_BUILD_DIR}/release64"
+  BUILD_DIR_DEBUG="${ROOT_BUILD_DIR}/debug64"
 fi
+
+echo "Build dirs: ${BUILD_DIR_RELEASE}"
+echo "Build dirs: ${BUILD_DIR_DEBUG}"
 
 
 build_target ${BUILD_DIR_RELEASE} "release"
