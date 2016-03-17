0a7cf31512cc94db566a2849525730bc73e566cb
jonolafur <jonolafur@b3ce3c30-b956-4919-a679-e7ed1d85cd78>
2016-03-17 14:18:42 +0000

    Corrected batch processing. Now using many small ffts and accumulating magnitude

--- src/fftw_vector.h
+++ src/fftw_vector.h
@@ -34,75 +34,76 @@
 class fftw_vector
 {
 public:
-	fftw_complex*   m_x;
+   fftw_complex*   m_x;
 
 protected:
-	fftw_plan       m_pFwdPlan, m_pBwdPlan;
-	std::size_t     m_size;
-	std::size_t     m_idx;
-	int             m_plan_mode;
-	double          m_Df;  // Frequency bin size [Hz]
-	double          m_Dt;  // Sample time [s]
+   fftw_plan       m_pFwdPlan, m_pBwdPlan;
+   std::size_t     m_size;
+   std::size_t     m_idx;
+   int             m_plan_mode;
+   double          m_Df;  // Frequency bin size [Hz]
+   double          m_Dt;  // Sample time [s]
 
 public:
-	fftw_vector() : m_x(0L), m_pFwdPlan(),m_pBwdPlan(), m_size(0L),
-                   m_idx(0L), m_plan_mode(FFTW_MEASURE),m_Df(1.0), m_Dt(1.0) {}
-	virtual ~fftw_vector();
-private:
-	fftw_vector(const fftw_vector& ): m_x(0L), m_pFwdPlan(),m_pBwdPlan(), m_size(0L),
-                                     m_idx(0L), m_plan_mode(FFTW_MEASURE),m_Df(1.0), m_Dt(1.0){}
-	fftw_vector& operator=(const fftw_vector& ){return *this;}
-public:
-	void re_alloc( std::size_t N, bool fftw_estimate);
-	double norm() const {return sqrt(normSquare());}
-	double normSquare() const;
-	void normalize(){ normalizeToValue(sqrt(static_cast<double>(m_size))); }
-	void normalizeSquare(){ normalizeToValue(static_cast<double>(m_size)); }
-	void normalizeToValue(double norm);
-	void fft();
-	void ifft();
-	void acf(bool removeBartlettWindow=false);
-	void acf_normalized(bool removeBartlettWindow=false);
-	void fft_filter(double bw, double f_s);
-	void setSampleTime(double dt);
-	void setSampleFrequency(double df);
-	bool full(){return m_idx == m_size;}
+   fftw_vector();
+   fftw_vector(size_t size, double real, double imag);
+   fftw_vector(fftw_vector& fft_vec);
 
-	std::size_t size() const {return m_size;}
-	double frequency(int i) const {return static_cast<double>(i)*m_Df;}
-	double time(int i) const {return static_cast<double>(i)*m_Dt;}
+   virtual ~fftw_vector();
+
+public:
+   void re_alloc( std::size_t N, bool fftw_estimate = FFTW_ESTIMATE);
+   double norm() const {return sqrt(normSquare());}
+   double normSquare() const;
+   void normalize(){ normalizeToValue(sqrt(static_cast<double>(m_size))); }
+   void normalizeSquare(){ normalizeToValue(static_cast<double>(m_size)); }
+   void normalizeToValue(double norm);
+   void fft();
+   void ifft();
+   void acf(bool removeBartlettWindow=false);
+   void acf_normalized(bool removeBartlettWindow=false);
+   void fft_filter(double bw, double f_s);
+   void setSampleTime(double dt);
+   void setSampleFrequency(double df);
+   bool full(){return m_idx == m_size;}
+   void initAll(double real, double imag);
+
+   std::size_t size() const {return m_size;}
+   double frequency(int i) const {return static_cast<double>(i)*m_Df;}
+   double time(int i) const {return static_cast<double>(i)*m_Dt;}
    double sampleTime() const {return m_Dt;}
    double sampleFrequency() const {return m_Df;}
    void write(const std::string& file_name, bool freq=true);
-	polar getPolar(int idx);
-	double abs(int idx) const {return sqrt(m_x[idx][0]*m_x[idx][0]+ m_x[idx][1]*m_x[idx][1]);}
-	double arg(int idx) const {return atan2(m_x[idx][1],m_x[idx][0]);}
-	double real(int idx) const {return m_x[idx][0];}
-	double imag(int idx) const {return m_x[idx][1];}
-	void getSample(double& x, double& y, int idx, bool polar_coord);
+   polar getPolar(int idx);
+   double abs(int idx) const {return sqrt(m_x[idx][0]*m_x[idx][0]+ m_x[idx][1]*m_x[idx][1]);}
+   double arg(int idx) const {return atan2(m_x[idx][1],m_x[idx][0]);}
+   double real(int idx) const {return m_x[idx][0];}
+   double imag(int idx) const {return m_x[idx][1];}
+   void getSample(double& x, double& y, int idx, bool polar_coord);
 
-	fftw_complex& operator[](std::size_t i){return m_x[i];}
+   fftw_complex& operator[](std::size_t i){return m_x[i];}
+   fftw_vector& operator=(const fftw_vector& rhs);
 
-	// Element-wise complex conjugate multiplication: z = z \cdot \bar{w}. z is *this.
-	// Please note that the parameter conjugate_result refer to the _result_ not to the operation itself.
-	void mult_conj(fftw_vector& p, bool conjugate_result=false);
-	void mult_conj(const fftw_complex& p, bool conjugate_result=false);
+   // Element-wise complex conjugate multiplication: z = z \cdot \bar{w}. z is *this.
+   // Please note that the parameter conjugate_result refer to the _result_ not to the operation itself.
+   void mult_conj(fftw_vector& p, bool conjugate_result=false);
+   void mult_conj(const fftw_complex& p, bool conjugate_result=false);
 
-	void set_samples(const std::vector<double>& real, const std::vector<double>& imag, bool fftw_estimate);
+   void set_samples(const std::vector<double>& real, const std::vector<double>& imag, bool fftw_estimate);
 
    void push_back(const double real, const double imag);
 
    template<typename Sample_Type>
-		void push_back(const Sample_Type& sample);
+      void push_back(const Sample_Type& sample);
 
-	template<typename Sample_Type>
-		void set_samples(const std::vector<Sample_Type>& vec, bool fftw_estimate);
+   template<typename Sample_Type>
+      void set_samples(const std::vector<Sample_Type>& vec, bool fftw_estimate);
 
 private:
-	void normalizeACF();
-	void free_samples();
-	void copy_sample(fftw_complex& z, double x) const;
-	void copy_sample(fftw_complex& z, const fftw_complex& rhs) const;
+   void normalizeACF();
+   void free_samples();
+   void copy_sample(fftw_complex& z, double x) const;
+   void copy_sample(fftw_complex& z, const fftw_complex& rhs) const;
    void removeImplicitBartlettWindow();
    void acf_unscaled(bool removeBartlettWindow);
 };
@@ -115,20 +116,20 @@ private:
 template<typename Sample_Type>
 void fftw_vector::set_samples( const std::vector< Sample_Type >& vec, bool fftw_estimate)
 {
-	re_alloc( vec.size(), fftw_estimate );
+   re_alloc( vec.size(), fftw_estimate );
 
-	for(std::size_t i=0; i<m_size; i++)
-		copy_sample(m_x[i], vec[i]);
+   for(std::size_t i=0; i<m_size; i++)
+      copy_sample(m_x[i], vec[i]);
 }
 ///////////////////////////////////////////////////////////////////////////////
 template<typename Sample_Type>
 void fftw_vector::push_back(const Sample_Type& sample)
 {
-	if(m_idx<m_size)
-	{
-		copy_sample(m_x[m_idx], sample);
-		m_idx++;
-	}
+   if(m_idx<m_size)
+   {
+      copy_sample(m_x[m_idx], sample);
+      m_idx++;
+   }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- src/main.cpp
+++ src/main.cpp
@@ -15,92 +15,92 @@
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-	const int ret_error = 1;
-	const int ret_ok = 0;
+   const int ret_error = 1;
+   const int ret_ok = 0;
 
    LogStreamHandler log_stream_handler;
    
    log_stream_handler.redirect_clog_toFileInHomeDir(".fft.log");
    
-	int ret_val = ret_ok;
-	std::string programName("fft");
-	
-	try
-	{
-		logHistory(argc, argv);
+   int ret_val = ret_ok;
+   std::string programName("fft");
+   
+   try
+   {
+      logHistory(argc, argv);
 
-		fftOptions opt(argc, argv);
+      fftOptions opt(argc, argv);
 
-		if(opt.writeVersionToConsole(programName))
-			return ret_ok;
+      if(opt.writeVersionToConsole(programName))
+         return ret_ok;
 
-		if(opt.writeHelpToConsole())
-			return ret_ok;
-		
-		process(opt);
+      if(opt.writeHelpToConsole())
+         return ret_ok;
+      
+      process(opt);
 
    }
-	catch(std::exception& e)
-	{
-		std::clog << "Error: " << e.what() << std::endl;
-		ret_val = ret_error;
-	}
-	catch(std::string& s)
-	{
-		std::clog << "Error: " << s << std::endl;
-		ret_val = ret_error;
-	}
-	catch(const char* s)
-	{
-		std::clog << "Error: " << s << std::endl;
-		ret_val = ret_error;
-	}
-	catch(...)
-	{
-		std::clog << "Exception of unknown type!\n";
-		ret_val = ret_error;
-	}
+   catch(std::exception& e)
+   {
+      std::clog << "Error: " << e.what() << std::endl;
+      ret_val = ret_error;
+   }
+   catch(std::string& s)
+   {
+      std::clog << "Error: " << s << std::endl;
+      ret_val = ret_error;
+   }
+   catch(const char* s)
+   {
+      std::clog << "Error: " << s << std::endl;
+      ret_val = ret_error;
+   }
+   catch(...)
+   {
+      std::clog << "Exception of unknown type!\n";
+      ret_val = ret_error;
+   }
 
-	if(ret_error)
-		std::cout << "# Errors were encountered, please see log file: " << log_stream_handler.fileName() << std::endl;
+   if(ret_error)
+      std::cout << "# Errors were encountered, please see log file: " << log_stream_handler.fileName() << std::endl;
 
-	return ret_val;
+   return ret_val;
 }
 ///////////////////////////////////////////////////////////////////////////////
 void process(fftOptions& opt)
 {
-	fftw_vector fft_vec;
-	StreamHandler stream_handler(opt.inputFile());
+   fftw_vector fft_vec;
+   StreamHandler stream_handler(opt.inputFile());
 
-	readInput(opt, fft_vec, stream_handler);
-	processInput(opt,fft_vec);
-	writeOutput(opt,fft_vec);
+   readInput(opt, fft_vec, stream_handler);
+   processInput(opt,fft_vec);
+   writeOutput(opt,fft_vec);
 }
 ///////////////////////////////////////////////////////////////////////////////
 void readInput(fftOptions& opt, fftw_vector& fft_vec, StreamHandler& stream_handler)
 {
-	data_columns fft_data;
+   data_columns fft_data;
 
-	std::vector<int> colIdx = opt.getZeroBasedColumnIndexes();
+   std::vector<int> colIdx = opt.getZeroBasedColumnIndexes();
 
-	getColsFromFile(stream_handler, fft_data, colIdx, opt.getDelimiterAsString() );
+   getColsFromFile(stream_handler, fft_data, colIdx, opt.getDelimiterAsString() );
 
-	if(opt.zeroPadSamples())
-		fft_data.zeroPadSamplesByFactorTwo();
+   if(opt.zeroPadSamples())
+      fft_data.zeroPadSamplesByFactorTwo();
 
-	if(opt.isComplex())
-		fft_vec.set_samples(fft_data.realAxis(), fft_data.imagAxis(), opt.estimate_fftw());
-	else
-		fft_vec.set_samples(fft_data.realAxis(), opt.estimate_fftw());
+   if(opt.isComplex())
+      fft_vec.set_samples(fft_data.realAxis(), fft_data.imagAxis(), opt.estimate_fftw());
+   else
+      fft_vec.set_samples(fft_data.realAxis(), opt.estimate_fftw());
 
-	if(opt.xAxisSpecified())
-	{
-		double dx =  checkSampleDelta(fft_data.x_axis());
-		if(opt.inverseFFT())
-			fft_vec.setSampleFrequency(dx);
-		else
-			fft_vec.setSampleTime(dx);
-	}
+   if(opt.xAxisSpecified())
+   {
+      double dx =  checkSampleDelta(fft_data.x_axis());
+      if(opt.inverseFFT())
+         fft_vec.setSampleFrequency(dx);
+      else
+         fft_vec.setSampleTime(dx);
+   }
 
    if(opt.sampleWasRateSpecified())
       fft_vec.setSampleFrequency(opt.getSampleRate());
@@ -108,120 +108,136 @@ void readInput(fftOptions& opt, fftw_vector& fft_vec, StreamHandler& stream_hand
 ///////////////////////////////////////////////////////////////////////////////
 void processInput(fftOptions& opt, fftw_vector& fft_vec)
 {
-	if(opt.acf())
-	{
-		fft_vec.acf(opt.removeBartlett());
+   if(opt.acf())
+   {
+      fft_vec.acf(opt.removeBartlett());
 
-		if( opt.normalize() )
-			fft_vec.normalizeSquare();
+      if( opt.normalize() )
+         fft_vec.normalizeSquare();
 
-		return;
-	}
+      return;
+   }
 
-	if(opt.inverseFFT())
-		fft_vec.ifft();
-	else
-		fft_vec.fft();
-
-	if( opt.normalize() )
-		fft_vec.normalize();
-   
    if(opt.getNumBatches() > 1)
    {
       size_t n = fft_vec.size()/opt.getNumBatches();
-      vector<double> re(n,0.0);
+      size_t N = fft_vec.size();
+      double dt = fft_vec.sampleTime();
+      
+      fftw_vector vec(n, 0.0, 0.0), result_vec(n, 0.0, 0.0);
 
-      size_t j=0;
-      for(size_t i=0; i<n; i++)
+      for(size_t i=0; i<N; i++)
       {
-         for(size_t k=0; k<opt.getNumBatches(); k++)
-            re[i] += fft_vec.abs(j++);
+         vec[i%n][0] = fft_vec[i][0];
+         vec[i%n][1] = fft_vec[i][1];
+
+         if( (i+1)%n == 0 )
+         {
+            if(opt.inverseFFT())
+               vec.ifft();
+            else
+               vec.fft();
+
+            for(size_t k=0; k<n; k++)
+               fftw_add(result_vec[k], vec.abs(k));
+         }
       }
-      fft_vec.set_samples(re, true);
+      fft_vec = result_vec;
+
+      fft_vec.setSampleTime( dt );
+      return;
    }
+
+   if(opt.inverseFFT())
+      fft_vec.ifft();
+   else
+      fft_vec.fft();
+
+   if( opt.normalize() )
+      fft_vec.normalize();
 }
-///////////////////////////////////////////////////////////////////////////////
+
+
 void writeOutput(fftOptions& opt, fftw_vector& fft_vec)
 {
-	std::ostream* out_stream;
-	std::ofstream out_file;
+   std::ostream* out_stream;
+   std::ofstream out_file;
 
-	// Re-direct to standard out if no filename is given:
-	if(!opt.outputFile().empty())
-	{
-		out_file.open(opt.outputFile().c_str());
-		out_stream = &out_file;
-	}
-	else
-		out_stream = &std::cout;
+   // Re-direct to standard out if no filename is given:
+   if(!opt.outputFile().empty())
+   {
+      out_file.open(opt.outputFile().c_str());
+      out_stream = &out_file;
+   }
+   else
+      out_stream = &std::cout;
 
-	(*out_stream).precision(12);
+   (*out_stream).precision(12);
 
-	if(opt.orderSamples())
-		writeOrdered(fft_vec, out_stream, opt);
+   if(opt.orderSamples())
+      writeOrdered(fft_vec, out_stream, opt);
    else
       writeStandard(fft_vec, out_stream, opt );
 }
 ///////////////////////////////////////////////////////////////////////////////
 double checkSampleDelta(const std::vector<double>& t)
 {
-	std::size_t N = t.size();
-	if(N<2) throw "An x-axis was specified on the command line (option -x), but in "
-			"that column less than two samples were found.";
+   std::size_t N = t.size();
+   if(N<2) throw "An x-axis was specified on the command line (option -x), but in "
+         "that column less than two samples were found.";
 
-	double dt = ( t.back()-t.front() )/static_cast<double>(N-1);
-	// TODO: generate some statistics on the time axis
-	// (max/min deviation from average sample time etc.)
-	return dt;
+   double dt = ( t.back()-t.front() )/static_cast<double>(N-1);
+
+   return dt;
 }
 ///////////////////////////////////////////////////////////////////////////////
 void writeOrdered(fftw_vector& fft, std::ostream* out_stream, const fftOptions& opt)
 {
-	int N = static_cast<int>(fft.size());
+   int N = static_cast<int>(fft.size());
 
-	// The negative half-axis:
-	for(int i=N/2; i<N; i++)
-		writeSample(i, N, fft,  out_stream, opt);
+   // The negative half-axis:
+   for(int i=N/2; i<N; i++)
+      writeSample(i, N, fft,  out_stream, opt);
 
-	for(int i=0; i<N/2; i++)
-		writeSample(i, 0, fft,  out_stream, opt);
+   for(int i=0; i<N/2; i++)
+      writeSample(i, 0, fft,  out_stream, opt);
 }
 ///////////////////////////////////////////////////////////////////////////////
 void writeStandard(fftw_vector& fft, std::ostream* out_stream, const fftOptions& opt)
 {
-	int N = static_cast<int>(fft.size());
+   int N = static_cast<int>(fft.size());
 
-	if( opt.positiveAxisOnly() ) N/=2;
+   if( opt.positiveAxisOnly() ) N/=2;
 
-	for(int i=0; i<N; i++)
-		writeSample(i, 0, fft, out_stream, opt);
+   for(int i=0; i<N; i++)
+      writeSample(i, 0, fft, out_stream, opt);
 }
 ///////////////////////////////////////////////////////////////////////////////
 void writeSample(int idx, int offset, fftw_vector& fft, std::ostream* out_stream, const fftOptions& opt)
 {
-	double a,x,y;
+   double a,x,y;
 
-	// If an IFFT was performed, the assumption is that the horizontal axis of
-	// the _output_ (i.e. _after_ the transformation) is a time coordinate
-	if(opt.inverseFFT() || opt.acf())
-		a = fft.time(idx-offset);
-	else // Otherwise it is assumed to be a frequency coordinate.
-		a = fft.frequency(idx-offset);
+   // If an IFFT was performed, the assumption is that the horizontal axis of
+   // the _output_ (i.e. _after_ the transformation) is a time coordinate
+   if(opt.inverseFFT() || opt.acf())
+      a = fft.time(idx-offset);
+   else // Otherwise it is assumed to be a frequency coordinate.
+      a = fft.frequency(idx-offset);
 
-	fft.getSample(x, y, idx, opt.writeMagnitudeAndPhase() );
+   fft.getSample(x, y, idx, opt.writeMagnitudeAndPhase() );
 
-	(*out_stream) << a << ' ' << x << ' ' << y << '\n';
+   (*out_stream) << a << ' ' << x << ' ' << y << '\n';
 }
 ///////////////////////////////////////////////////////////////////////////////
 void logHistory( int a, char* av[] )
 {
-	bpt::ptime t(bpt::second_clock::local_time());
+   bpt::ptime t(bpt::second_clock::local_time());
 
-	std::clog << "# " << t << std::endl;
-	// Print out the options, skip the program name...
-	for(int i=1; i<a;++i)
-		std::clog << av[i] << ' ';
-	std::clog << std::endl;
+   std::clog << "# " << t << std::endl;
+   // Print out the options, skip the program name...
+   for(int i=1; i<a;++i)
+      std::clog << av[i] << ' ';
+   std::clog << std::endl;
 }
 
 
